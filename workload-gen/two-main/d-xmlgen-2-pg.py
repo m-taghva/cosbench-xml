13257d3f847d303c0e666197027a5e955f3794a2
Mohammad Taghva <105096486+m-taghva@users.noreply.github.com>
2023-09-09 13:39:21 +0330

    Update d-xmlgen-2-pg.py

--- workload-gen/two-main/d-xmlgen-2-pg.py
+++ workload-gen/two-main/d-xmlgen-2-pg.py
@@ -1,13 +1,24 @@
 import xml.etree.ElementTree as ET
 import copy
 import os
+import sys
 
 class bcolors:
     YELLOW = '\033[1;33m'
     END = '\033[0m'
 
-# Get user input for the XML file path
-xml_file_path = input("Enter the path to the XML configuration file: ")
+# Check if the correct number of command-line arguments are provided
+if len(sys.argv) != 3:
+    print("Usage: python script.py <xml_file> <max_number>")
+    exit(1)
+
+# Get the XML file path and maximum number from command-line arguments
+xml_file_path = sys.argv[1]
+try:
+    max_number = int(sys.argv[2])
+except ValueError:
+    print("Invalid input for max_number. Please provide a valid number.")
+    exit(1)
 
 # Parse the XML configuration file
 try:
@@ -18,16 +29,6 @@ except FileNotFoundError:
     exit(1)
 except ET.ParseError:
     print("Invalid XML file. Please provide a valid XML file.")
-    exit(1)
-
-# Get user input for the maximum number of workers for both "main1" and "main2"
-try:
-    max_workers = int(input("Enter the maximum number of workers for both main1 and main2: "))
-    if max_workers < 1:
-        print("Maximum number of workers must be at least 1.")
-        exit(1)
-except ValueError:
-    print("Invalid input. Please enter a valid number for the maximum workers.")
     exit(1)
 
 # Extract the original XML file name (without extension)
@@ -41,7 +42,7 @@ workers_main2 = 1
 unique_combinations = set()
 
 # Create XML files with "main1" having a static 1 and "main2" in a range, counting in double (Type 1)
-while workers_main2 <= max_workers:
+while workers_main2 <= max_number:
     # Create a deep copy of the root element for each iteration
     new_root = copy.deepcopy(root)
     
@@ -54,7 +55,7 @@ while workers_main2 <= max_workers:
         work.set('workers', str(workers_main2))
     
     # Generate a new XML file name based on the specified format
-    updated_file_name = f"{original_file_name}-type1-p1-g{workers_main2}.xml"
+    updated_file_name = f"{original_file_name}-type1-p1-g{workers_main2}-d.xml"
     
     # Check if the combination has already been processed
     combination = (1, workers_main2)
@@ -77,7 +78,7 @@ workers_main1 = 1
 workers_main2 = 1
 
 # Create XML files with "main2" having a static 1 and "main1" in a range, counting in double (Type 2)
-while workers_main1 <= max_workers:
+while workers_main1 <= max_number:
     # Create a deep copy of the root element for each iteration
     new_root = copy.deepcopy(root)
     
@@ -90,7 +91,7 @@ while workers_main1 <= max_workers:
         work.set('workers', str(workers_main1))
     
     # Generate a new XML file name based on the specified format
-    updated_file_name = f"{original_file_name}-type2-p{workers_main1}-g1.xml"
+    updated_file_name = f"{original_file_name}-type2-p{workers_main1}-g1-d.xml"
     
     # Check if the combination has already been processed
     combination = (workers_main1, 1)
@@ -113,23 +114,23 @@ workers_main1 = 1
 workers_main2 = 1
 
 # Create XML files with "main1" having the maximum number and "main2" in a range, counting in double (Type 3)
-while workers_main2 <= max_workers:
+while workers_main2 <= max_number:
     # Create a deep copy of the root element for each iteration
     new_root = copy.deepcopy(root)
     
     # Find and update the number of workers in main1 (maximum)
     for work in new_root.findall(".//work[@name='main1']"):
-        work.set('workers', str(max_workers))
+        work.set('workers', str(max_number))
     
     # Find and update the number of workers in main2 (range, counting in double)
     for work in new_root.findall(".//work[@name='main2']"):
         work.set('workers', str(workers_main2))
     
     # Generate a new XML file name based on the specified format
-    updated_file_name = f"{original_file_name}-type3-p{max_workers}-g{workers_main2}.xml"
+    updated_file_name = f"{original_file_name}-type3-p{max_number}-g{workers_main2}-d.xml"
     
     # Check if the combination has already been processed
-    combination = (max_workers, workers_main2)
+    combination = (max_number, workers_main2)
     if combination not in unique_combinations:
         unique_combinations.add(combination)
         
@@ -139,7 +140,7 @@ while workers_main2 <= max_workers:
         tree_copy = ET.ElementTree(new_root)
         tree_copy.write(updated_file_path)
         
-        print(f"Type 3: XML configuration updated for main1={bcolors.YELLOW}{max_workers}{bcolors.END} and main2={bcolors.YELLOW}{workers_main2}{bcolors.END} workers and saved to {bcolors.YELLOW}{updated_file_name}{bcolors.END}")
+        print(f"Type 3: XML configuration updated for main1={bcolors.YELLOW}{max_number}{bcolors.END} and main2={bcolors.YELLOW}{workers_main2}{bcolors.END} workers and saved to {bcolors.YELLOW}{updated_file_name}{bcolors.END}")
     
     # Double the worker count for "main2" for the next iteration
     workers_main2 *= 2
@@ -149,23 +150,23 @@ workers_main1 = 1
 workers_main2 = 1
 
 # Create XML files with "main2" having the maximum number and "main1" in a range, counting in double (Type 4)
-while workers_main1 <= max_workers:
+while workers_main1 <= max_number:
     # Create a deep copy of the root element for each iteration
     new_root = copy.deepcopy(root)
     
     # Find and update the number of workers in main2 (maximum)
     for work in new_root.findall(".//work[@name='main2']"):
-        work.set('workers', str(max_workers))
+        work.set('workers', str(max_number))
     
     # Find and update the number of workers in main1 (range, counting in double)
     for work in new_root.findall(".//work[@name='main1']"):
         work.set('workers', str(workers_main1))
     
     # Generate a new XML file name based on the specified format
-    updated_file_name = f"{original_file_name}-type4-p{workers_main1}-g{max_workers}.xml"
+    updated_file_name = f"{original_file_name}-type4-p{workers_main1}-g{max_number}-d.xml"
     
     # Check if the combination has already been processed
-    combination = (workers_main1, max_workers)
+    combination = (workers_main1, max_number)
     if combination not in unique_combinations:
         unique_combinations.add(combination)
         
@@ -175,7 +176,7 @@ while workers_main1 <= max_workers:
         tree_copy = ET.ElementTree(new_root)
         tree_copy.write(updated_file_path)
         
-        print(f"Type 4: XML configuration updated for main1={bcolors.YELLOW}{workers_main1}{bcolors.END} and main2={bcolors.YELLOW}{max_workers}{bcolors.END} workers and saved to {bcolors.YELLOW}{updated_file_name}{bcolors.END}")
+        print(f"Type 4: XML configuration updated for main1={bcolors.YELLOW}{workers_main1}{bcolors.END} and main2={bcolors.YELLOW}{max_number}{bcolors.END} workers and saved to {bcolors.YELLOW}{updated_file_name}{bcolors.END}")
     
     # Double the worker count for "main1" for the next iteration
     workers_main1 *= 2
