dc7ee71bfdbc506df9749e16aa89cccc023e1880
Mohammad Taghva <105096486+m-taghva@users.noreply.github.com>
2023-09-10 09:29:10 +0330

    Update xmlgen.py

--- workload-gen/one-main/xmlgen.py
+++ workload-gen/one-main/xmlgen.py
@@ -6,8 +6,8 @@ class bcolors:
     YELLOW = '\033[1;33m'
     END = '\033[0m'
 
-# Define the static output directory one level above the current working directory
-output_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "all-xml"))
+# Define the output directory as an absolute path
+output_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),"..","all-xml"))
 
 def update_worker_count(xml_file, new_worker_count):
     tree = ET.parse(xml_file)
@@ -18,7 +18,7 @@ def update_worker_count(xml_file, new_worker_count):
     main_work.set('workers', str(new_worker_count))
 
     modified_xml_file = xml_file.replace('.xml', f'-w{new_worker_count}.xml')
-    modified_xml_path = os.path.join(output_dir, modified_xml_file)
+    modified_xml_path = os.path.join(output_dir, os.path.basename(modified_xml_file))
     
     tree.write(modified_xml_path, encoding='utf-8', xml_declaration='<?xml version="1.0" encoding="utf-8"?>')
 
@@ -27,7 +27,7 @@ if __name__ == "__main__":
         print("Usage: python script.py [input.xml] [max_worker_count]")
         sys.exit(1)
 
-    xml_file_name = sys.argv[1]
+    xml_file_path = os.path.abspath(sys.argv[1])  # Accept an absolute path to the XML file
     max_worker_count = int(sys.argv[2])
 
     # Create the output directory if it doesn't exist
@@ -36,7 +36,7 @@ if __name__ == "__main__":
     worker_counts = [2**i for i in range(int(max_worker_count.bit_length()))]
 
     for count in worker_counts:
-        update_worker_count(xml_file_name, count)
+        update_worker_count(xml_file_path, count)
         print(f"Modified XML for {bcolors.YELLOW}{count}{bcolors.END} workers saved in {output_dir}.")
 
     print("All modifications complete.")
