241d35ad5f3415c5ee005f2750c7d646a82895cb
Mohammad Taghva <105096486+m-taghva@users.noreply.github.com>
2023-09-11 12:42:17 +0330

    Update wl-maker.py

--- workload-gen/wl-maker.py
+++ workload-gen/wl-maker.py
@@ -1,5 +1,4 @@
 import subprocess
-import sys
 
 def run_script(script_path, xml_path, a_number):
     try:
@@ -9,18 +8,18 @@ def run_script(script_path, xml_path, a_number):
         print(f"Error running {script_path}: {e}")
 
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        print("Usage: python new_script.py <script1_path> <xml1_path> <script2_path> <xml2_path> ... <a_number>")
-        sys.exit(1)
+    input_file = "wl-address.txt"
 
-    script_paths = sys.argv[1:-1]
-    a_number = int(sys.argv[-1])
+    with open(input_file, "r") as file:
+        lines = file.readlines()
 
-    if len(script_paths) % 2 != 0:
-        print("Error: You must provide an even number of arguments for script and XML pairs.")
-        sys.exit(1)
+    if len(lines) % 3 != 0:
+        print("Error: Input file should have sets of three lines (script path, XML path, and number).")
+        exit(1)
 
-    for i in range(0, len(script_paths), 2):
-        script_path = script_paths[i]
-        xml_path = script_paths[i + 1]
+    for i in range(0, len(lines), 3):
+        script_path = lines[i].strip()
+        xml_path = lines[i + 1].strip()
+        a_number = int(lines[i + 2].strip())
+
         run_script(script_path, xml_path, a_number)
