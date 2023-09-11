36c8fab46c0529feb2973648888008cf62330dc2
Mohammad Taghva <105096486+m-taghva@users.noreply.github.com>
2023-09-11 12:47:02 +0330

    Update wl-maker.py

--- workload-gen/wl-maker.py
+++ workload-gen/wl-maker.py
@@ -1,9 +1,13 @@
 import subprocess
 
+class bcolors:
+    YELLOW = '\033[1;33m'
+    END = '\033[0m'
+    
 def run_script(script_path, xml_path, a_number):
     try:
         subprocess.check_call(["python3", script_path, xml_path, str(a_number)])
-        print(f"Script {script_path} executed successfully.")
+        print(f"Script {bcolors.YELLOW}{script_path}{bcolors.END} executed successfully.")
     except subprocess.CalledProcessError as e:
         print(f"Error running {script_path}: {e}")
 
