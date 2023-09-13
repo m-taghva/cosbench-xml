5835ea170e6c04cb6d74d1cfac2defd94bde65cc
Mohammad Taghva <105096486+m-taghva@users.noreply.github.com>
2023-09-13 11:03:26 +0330

    Update xmlgen-pg-x3.py

--- workload-gen/two-main-x3/xmlgen-pg-x3.py
+++ workload-gen/two-main-x3/xmlgen-pg-x3.py
@@ -2,6 +2,9 @@ import xml.etree.ElementTree as ET
 import copy
 import os
 import sys
+import random
+import string
+import re
 
 class bcolors:
     YELLOW = '\033[1;33m'
@@ -10,28 +13,42 @@ class bcolors:
 # Define the static output directory one level above the current working directory
 output_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "all-xml"))
 
+# Function to generate a random prefix of length up to 10 characters from A to Z
+def random_prefix():
+    return ''.join(random.choice(string.ascii_uppercase) for _ in range(random.randint(1, 10)))
+
+# Function to replace oprefix with a random value in the XML content
+def replace_oprefix(xml_content):
+    return re.sub(r'oprefix=[A-Za-z]+', f'oprefix={random_prefix()}', xml_content)
+
 def update_worker_counts(xml_file, max_workers_main1):
-    tree = ET.parse(xml_file)
-    root = tree.getroot()
+    with open(xml_file, "r") as file:
+        xml_template = file.read()
 
     original_file_name = os.path.splitext(os.path.basename(xml_file))[0]
 
     workers_main1 = 1
     while workers_main1 <= max_workers_main1:
         workers_main2 = workers_main1 * 3
-        new_root = copy.deepcopy(root)
 
-        for work in new_root.findall(".//work[@name='main1']"):
+        tree = ET.ElementTree(ET.fromstring(xml_template))
+        root = tree.getroot()
+        
+        # Replace oprefix with a random value
+        xml_template = replace_oprefix(xml_template)
+
+        for work in root.findall(".//work[@name='main1']"):
             work.set('workers', str(workers_main1))
 
-        for work in new_root.findall(".//work[@name='main2']"):
+        for work in root.findall(".//work[@name='main2']"):
             work.set('workers', str(workers_main2))
 
         updated_file_name = f"{original_file_name}-p{workers_main1}-g{workers_main2}.xml"
         updated_file_path = os.path.join(output_dir, updated_file_name)
 
-        tree_copy = ET.ElementTree(new_root)
-        tree_copy.write(updated_file_path)
+        with open(updated_file_path, "w") as updated_file:
+            # Convert the XML content to a string before writing it
+            updated_file.write(ET.tostring(root, encoding="utf-8").decode("utf-8"))
 
         #print(f"XML configuration updated for main1={bcolors.YELLOW}{workers_main1}{bcolors.END} and main2={bcolors.YELLOW}{workers_main2}{bcolors.END} workers and saved to {updated_file_name}.")
 
