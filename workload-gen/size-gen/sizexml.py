de626960f84b605ca9a5bc02913163a124e7f103
Mohammad Taghva <105096486+m-taghva@users.noreply.github.com>
2023-09-20 14:43:05 +0330

    Update sizexml.py

--- workload-gen/size-gen/sizexml.py
+++ workload-gen/size-gen/sizexml.py
@@ -3,6 +3,7 @@ import sys
 import os
 import random
 import string
+import glob
 
 # Function to generate a random prefix of length up to 10 characters from A to Z
 def random_prefix():
@@ -14,40 +15,51 @@ def replace_oprefix(xml_content, oprefix):
 
 # Check for command-line arguments
 if len(sys.argv) != 3:
-    print("Usage: script.py <path_to_xml_file> <max_size>")
+    print("Usage: script.py <path_to_xml_file_or_directory> <max_size>")
     sys.exit(1)
 
-# Get the path to the XML file and the maximum size from command-line arguments
-xml_path = sys.argv[1]
+# Get the path to the XML file or directory and the maximum size from command-line arguments
+input_path = sys.argv[1]
 max_size = int(sys.argv[2])
-
-# Read the template XML file
-with open(xml_path, "r") as file:
-    xml_template = file.read()
 
 # Create a directory to store the output XML files in the current directory
 output_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "all-xml"))
 os.makedirs(output_dir, exist_ok=True)
 
-# Start with size as 1 and double it until it exceeds the specified maximum size
-size = 1
-while size <= max_size:
+# Function to process a single XML file
+def process_single_xml(xml_path, size):
+    with open(xml_path, "r") as file:
+        xml_template = file.read()
+
     # Generate a new random oprefix for each file
     oprefix = random_prefix()
-    
+
     # Replace oprefix in the XML content with the random oprefix
     xml_template = replace_oprefix(xml_template, oprefix)
-    
+
     # Replace sizes in the XML content
     xml_content = re.sub(r'sizes=c\(\d+\)KB', f'sizes=c({size})KB', xml_template)
-    
+
     # Write the modified XML content to a new file in the "all-xml" directory
     output_file_name = f"{os.path.splitext(os.path.basename(xml_path))[0]}-sizes-{size}.xml"
     output_file_path = os.path.join(output_dir, output_file_name)
     with open(output_file_path, "w") as file:
         file.write(xml_content)
-    
-    # Double the size for the next iteration
-    size *= 2
+
+# Check if the input path is a directory or a single file
+if os.path.isdir(input_path):
+    # Process all XML files in the directory
+    xml_files = glob.glob(os.path.join(input_path, "*.xml"))
+    for xml_file in xml_files:
+        size = 1
+        while size <= max_size:
+            process_single_xml(xml_file, size)
+            size *= 2
+else:
+    # Process the single XML file
+    size = 1
+    while size <= max_size:
+        process_single_xml(input_path, size)
+        size *= 2
 
 #print(f"XML templates with sizes in powers of 2 and unique random oprefix values have been created in the '{output_dir}' directory.")
